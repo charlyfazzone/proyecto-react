8e0db4138018d18711e1d4fa0d5f8c6f62a08d44
Carlos Fazzone <carlosfazzone7@gmail.com>
2025-10-19 16:45:52 -0300

    semodifoco el router

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,26 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "../views/Login";
-import { Messages } from "../views/Messages";
+import Message from "../views/Messages";
 import { NotFound } from "../views/NotFound";
 import ProtectedRoute from "../components/ProtectedRoute";
 import Help from "../views/Help";
+
 
 const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/chat"
-          element={
-            <ProtectedRoute>
-              <Messages />
-            </ProtectedRoute>}
-        />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/chat" element={<Message />} />
+        </Route>
         <Route path="/help" element={<Help />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
+
 
 export { RouterApp }
