91111acc5635d626aaeeee4195c06de4443afb0f
Carlos Fazzone <carlosfazzone7@gmail.com>
2025-10-19 16:43:29 -0300

    se agrego tema contexto

---
+++ src/context/ThemeContex.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+
+export const ThemeContext = createContext(); // 👈 exportación nombrada
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () =>
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
