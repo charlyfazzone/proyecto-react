7b0bbc885909eaf38abfd54f12537bf7e3df188c
Carlos Fazzone <carlosfazzone7@gmail.com>
2025-10-21 18:15:43 -0300

    finalizacion

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
-import { Navigate } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
   console.log("Hola desde protected")
   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true"
 
-  return isLoggedIn ? children : <Navigate to="/" replace />
+  return isLoggedIn ? <Outlet /> : <Navigate to="/" replace />
 }
 
 export default ProtectedRoute
